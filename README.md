8b9bbdd73619985532765ac35684272c0de08880
Jesse Connell <ancon@upenn.edu>
2020-11-09 12:21:23 -0500

    tidy comparison of SONAR to GenBank alleles

--- README.md
+++ README.md
@@ -3,15 +3,18 @@
 Where is `IGHV2-ABO*01` in SONAR's germDB files?  Ryan found that that was a
 close match for RM6561's DH1021 lineage, but it's not in the IgDiscover results
 and in turn is not in the initial Ramesh database supplied by SONAR even though
-it's visible in GenBank. I should compare the original Ramesh files in GenBank
-with what's in SONAR's copy, in a systematic way.
+it's visible in GenBank.  The Ramesh paper:
 
 <https://www.frontiersin.org/articles/10.3389/fimmu.2017.01407/full>
 
-accessions MF989451 to MF989952
-
-First glance: downloading a FASTA per accession does *not* reveal
-`IGHV2-ABO*01` despite that allele being shown in the GenBank entry for
-MF989491.1.  It looks like that's one of the combo entries labeled "locus" and
-there isn't a separate "gene" one for it.  Is that why it's not in Chaim's set,
-maybe?
+The Snakemake rules download GBF files for MF989451 to MF989952 and extract
+individual FASTA sequences for each feature, and the `analysis.R` file parses
+out individual columns from the allele names and description text.  When
+filtering to just complete sequences (Partial == FALSE) the numbers match
+what's in the paper, but still not what's in SONAR's copy.  It doesn't look
+like a straightforward pattern to me, like sequences missing from the
+multi-gene accessions for example.  There are also some sequences in SONAR's
+copy that don't occur in the GenBank copy, specifically ones with an extra
+suffix on the name (like `IGKV3-ADU-S*01`) or possibly also with an "ORF"
+prefix like `ORF_IGKV1-AES-X*01`.  Some of that might be explained by removing
+those extra bits from the names but I'm not sure what they signify. 
--- analysis.R
+++ analysis.R
@@ -1,4 +1,16 @@
-genbank <- read.csv("converted/all.csv", stringsAsFactors = FALSE)
+
+# Helpers -----------------------------------------------------------------
+
+
+parse_groupings <- function(txt) {
+  result <- data.frame(
+    Gene = sub("^ORF_", "", sub("\\*.*$", "", txt)),
+    stringsAsFactors = FALSE)
+  result$Family <- sub("-.*$", "", result$Gene)
+  result$Segment <- sub("^(IG[HLK].).*$", "\\1", result$Family)
+  result$Locus <- substr(result$Gene, 1, 3)
+  result
+}
 
 parse_genbank_genes <- function(genbank) {
   genes <- subset(
@@ -7,10 +19,7 @@ parse_genbank_genes <- function(genbank) {
     select = c(feature_qualifier_gene, gbf_id, gbf_description, feature_seq))
   colnames(genes) <- c("Allele", "Accession", "AccessionDescription", "Seq")
   # Split out the ontological stuff
-  genes$Gene <- sub("\\*.*$", "", genes$Allele)
-  genes$Family <- sub("-.*$", "", genes$Gene)
-  genes$Segment <- sub("^(IG[HLK].).*$", "\\1", genes$Family)
-  genes$Locus <- substr(genes$Gene, 1, 3)
+  genes <- cbind(genes, parse_groupings(genes$Allele))
   # Also parse out details from the accession descriptions
   genes$Partial <- grepl("partial (cds|sequence)", genes$AccessionDescription)
   genes$Functional <- NA
@@ -18,4 +27,29 @@ parse_genbank_genes <- function(genbank) {
   genes
 }
 
-genes <- parse_genbank_genes(genbank)
+load_sonar_alleles <- function() {
+  sonar_alleles <- do.call(
+    rbind,
+    lapply(
+      list.files(
+        "SONAR/germDB", pattern="Ig(HD|HKL[VDJ])_BU_DD\\.fasta$",
+        full.names = TRUE), dnar::read.fa))
+  colnames(sonar_alleles) <- c("Allele", "Seq")
+  sonar_alleles <- cbind(sonar_alleles, parse_groupings(sonar_alleles$Allele))
+  sonar_alleles
+}
+
+
+# Go ----------------------------------------------------------------------
+
+
+genbank <- read.csv("converted/all.csv", stringsAsFactors = FALSE)
+genbank_alleles <- parse_genbank_genes(genbank)
+sonar_alleles <- load_sonar_alleles()
+
+# Across the board the SONAR set has fewer entries for each segment than the 
+# ones in GenBank from the paper.  Also, some of the names don't match up, with
+# an extra suffix on some of the SONAR ones.
+segments <- c(paste0("IGH", c("V", "D", "J")), "IGLV", "IGLJ", "IGKV", "IGKJ")
+table(subset(sonar_alleles, Segment %in% segments)$Segment)[segments]
+table(subset(genbank_alleles, Segment %in% segments & ! Partial)$Segment)[segments]
